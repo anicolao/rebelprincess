f7384c8371b69b66a3ec9e7b3cb96af36c8eeb82
anicolao <anicolao@users.noreply.github.com>
2026-07-16 19:03:52 +0000

    deploy: 5551ef15d2d7e98b1248bf3dcc8f737ed2bd69c1

---
+++ pr16/_app/immutable/nodes/0.gqekPHnH.js
@@ -0,0 +1,1 @@
+import{F as e,X as t,b as n,h as r,x as i}from"../chunks/Ba7scXEB.js";import"../chunks/xihTtKlq.js";var a=t({prerender:()=>!0,ssr:()=>!1});function o(t,a){var o=i();r(e(o),()=>a.children),n(t,o)}export{o as component,a as universal};
